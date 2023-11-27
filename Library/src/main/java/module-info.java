d8c58a54f062f47bdd72c0e71b2ea04a19d649af
Julian D <jpduran@calpoly.edu>
2023-11-27 11:45:55 -0800

    11/27

--- Library/src/main/java/module-info.java
+++ Library/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module project.finalproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens project.finalproject to javafx.fxml;
     exports project.finalproject;
--- query_classes/main.java
+++ query_classes/main.java
@@ -4,7 +4,7 @@ import java.sql.*;
 class Main {
     public static void main(String[] args) {
         init_connection con = new init_connection();
-        System.out.println(con.user_login("teddy", "pw2"));
+        System.out.println(con.user_login("jonsnow", "pw1"));
         System.out.println(con.employee_login("employee1jac", "emppw2"));
         con.close();
     }
